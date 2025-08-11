cafcc0be2e12507b217fbc94746e2d05b80acf12
Aler998 <alessandro.ercole98@gmail.com>
2025-08-11 15:41:58 +0200

    MQTT

--- mqtt/sensors.py
+++ mqtt/sensors.py
@@ -20,9 +20,9 @@ tca = adafruit_tca9548a.TCA9548A(i2c)
 
 # INIZIO DEBUG
 for channel in range(8):
+    print("Channel: 12345678")
     if tca[channel].try_lock():
-        print("Channel {}:".format(channel), end=
-        "")
+        print("Channel {}:".format(channel), end="")
         addresses = tca[channel].scan()
         print([hex(address) for address in addresses if address != 0x70])
         tca[channel].unlock()
